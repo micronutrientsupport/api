00457868cdc85d31db15166ab4d1274283e185dc
Bean, Andrew K <andan@bsg.ac.uk>
2020-11-13 11:31:50 +0000

    Introspect db to auto craete model for country table:
    Ensure datasource built:
      npm run build
      set -o allexport; source .env; set +o allexport
      lb4 discover

---
+++ src/models/country.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {idInjection: false, postgresql: {schema: 'bmgf', table: 'country'}}})
+export class Country extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+    id: 1,
+    postgresql: {columnName: 'id', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    postgresql: {columnName: 'name', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+  })
+  name?: string;
+
+  @property({
+    type: 'string',
+    postgresql: {columnName: 'geometry', dataType: 'USER-DEFINED', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+  })
+  geometry?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Country>) {
+    super(data);
+  }
+}
+
+export interface CountryRelations {
+  // describe navigational properties here
+}
+
+export type CountryWithRelations = Country & CountryRelations;
