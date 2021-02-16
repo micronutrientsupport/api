88a5e2dc22886ee0773c6a041e6d77762c2368dd
Bean, Andrew K <andan@bsg.ac.uk>
2021-02-16 14:54:53 +0000

    feat(opencpu): add opencpu datasource and example string replacement route

---
+++ src/controllers/opencpu-controller.controller.ts
@@ -0,0 +1,28 @@
+// Uncomment these imports to begin using these cool features!
+
+import {inject} from '@loopback/core';
+import {get, param} from '@loopback/rest';
+import {OcpuLibrary, OpencpuService} from '../services';
+
+
+export class OpencpuControllerController {
+  constructor(
+    @inject('services.OpencpuService')
+    protected opencpuService: OpencpuService,
+  ) { }
+
+  @get('/library/{string}/{pattern}/{replacement}')
+  async getStarWarCharacter(
+    @param.path.string('string') string: string,
+    @param.path.string('pattern') pattern: string,
+    @param.path.string('replacement') replacement: string
+  ): Promise<OcpuLibrary> {
+    return await this.opencpuService.replace(string, pattern, replacement);
+  }
+
+  @get('/lib')
+  async getLib(): Promise<string[]> {
+    let lib = await this.opencpuService.library();
+    return lib.split("\n");
+  }
+}
---
+++ src/datasources/opencpu.datasource.ts
@@ -0,0 +1,58 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'opencpu',
+  connector: 'rest',
+  baseURL: 'https://opencpu.micronutrient.support/ocpu/',
+  options: {
+    //headers: {
+    //  accept: "application/json",
+    //  "content-type": "application/json"
+    //},
+  },
+  operations: [
+    {
+      template: {
+        method: "POST",
+        url: "https://opencpu.micronutrient.support/ocpu/library/stringr/R/str_replace/json",
+        body: {
+          string: "{string:string}",
+          pattern: "{pattern:string}",
+          replacement: "{replacement}"
+        },
+      },
+      functions: {
+        replace: ["string", "pattern", "replacement"]
+      },
+    },
+    {
+      template: {
+        method: "GET",
+        url: "https://opencpu.micronutrient.support/ocpu/library",
+      },
+      functions: {
+        library: []
+      },
+    }
+  ],
+  crud: false
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class OpencpuDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'opencpu';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.opencpu', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
