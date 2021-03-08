7d0e49570e11b8013aea99792c2f3acd6b6d5e23
Bean, Andrew K <andan@bsg.ac.uk>
2021-03-08 10:45:07 +0000

    feat: add top 20 route for household level data

--- src/controllers/diet-country-controller.controller.ts
+++ src/controllers/diet-country-controller.controller.ts
@@ -2,8 +2,8 @@
 
 import {Filter, repository} from '@loopback/repository';
 import {get, getModelSchemaRef, param} from '@loopback/rest';
-import {Top20MnPerCountry} from '../models';
-import {Top20MnPerCountryRepository} from '../repositories';
+import {Top20MnPerCountry, Top20MnPerHhsurvey} from '../models';
+import {Top20MnPerCountryRepository, Top20MnPerHhsurveyRepository} from '../repositories';
 import {StandardJsonResponse} from './standardJsonResponse';
 import {StandardOpenApiResponses} from './standardOpenApiResponses';
 
@@ -14,7 +14,36 @@ export class DietCountryControllerController {
   constructor(
     @repository(Top20MnPerCountryRepository)
     public top20MnPerCountryRepository: Top20MnPerCountryRepository,
+    @repository(Top20MnPerHhsurveyRepository)
+    public top20MnPerHhsurveyRepository: Top20MnPerHhsurveyRepository,
   ) { }
+
+  @get('/diet/household/top20/{countryId}/{micronutrientId}/{compositionId}/{consumptionId}', {
+    responses:
+      new StandardOpenApiResponses('Data sources')
+        .setDataType('array')
+        .setObjectSchema(getModelSchemaRef(Top20MnPerHhsurvey))
+        .toObject(),
+  })
+  async findTop20FoodsHousehold(
+    @param.path.string('countryId') countryId: string,
+    @param.path.string('micronutrientId') micronutrientId: string,
+    @param.path.string('compositionId') compositionId: number,
+    @param.path.string('consumptionId') consumptionId: number): Promise<object> {
+    let filter: Filter = {
+      where: {
+        //countryId: countryId,
+        mnName: micronutrientId,
+        fctSourceId: compositionId,
+        surveyId: consumptionId
+      }
+    };
+    let data = await this.top20MnPerHhsurveyRepository.find(filter)
+    return new StandardJsonResponse<Array<Top20MnPerHhsurvey>>(
+      `${data.length} top food results returned.`,
+      data,
+    );
+  }
 
   @get('/diet/country/top20/{countryId}/{micronutrientId}/{compositionId}/{consumptionId}', {
     responses:
@@ -23,7 +52,7 @@ export class DietCountryControllerController {
         .setObjectSchema(getModelSchemaRef(Top20MnPerCountry))
         .toObject(),
   })
-  async findFoodGroupScenario(
+  async findTop20FoodsCountry(
     @param.path.string('countryId') countryId: string,
     @param.path.string('micronutrientId') micronutrientId: string,
     @param.path.string('compositionId') compositionId: number,
