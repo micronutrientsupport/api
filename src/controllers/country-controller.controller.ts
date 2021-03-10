bcf84b58e2ab677ec5a547c5bc7254357b1c68ae
Bean, Andrew K <andan@bsg.ac.uk>
2021-03-10 14:42:36 +0000

    Add household level geojson route

--- src/controllers/country-controller.controller.ts
+++ src/controllers/country-controller.controller.ts
@@ -15,7 +15,7 @@ export class CountryController {
   constructor(
     @repository(CountryRepository)
     public countryRepository: CountryRepository,
-  ) {}
+  ) { }
 
   @get('/country', {
     responses:
