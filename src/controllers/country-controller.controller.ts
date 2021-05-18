ef53b6109f5849cca424990e4e5d823779f4be9e
Bean, Andrew K <andan@bsg.ac.uk>
2021-05-18 13:09:54 +0100

    feat(cache): Add decorator to set cache expire header on endpoints

--- src/controllers/country-controller.controller.ts
+++ src/controllers/country-controller.controller.ts
@@ -1,11 +1,15 @@
+import {inject} from '@loopback/core';
 import {
 
   repository
 } from '@loopback/repository';
 import {
   get,
-  getModelSchemaRef
+  getModelSchemaRef,
+
+  Response, RestBindings
 } from '@loopback/rest';
+import {CacheHeader} from '../decorators/cache-header.decorator';
 import {Country} from '../models';
 import {CountryRepository} from '../repositories';
 import {StandardJsonResponse} from './standardJsonResponse';
@@ -15,8 +19,10 @@ export class CountryController {
   constructor(
     @repository(CountryRepository)
     public countryRepository: CountryRepository,
+    @inject(RestBindings.Http.RESPONSE) public response: Response
   ) { }
 
+  @CacheHeader(600)
   @get('/country', {
     responses:
       new StandardOpenApiResponses('Array of Country model instances')
