9269a854d9ca0817c96b6175d880349bac4ff2fc
Bean, Andrew K <andan@bsg.ac.uk>
2021-08-29 16:31:08 +0100

    fix: consolidation and docs updates

---
+++ src/controllers/diet/data-sources/data-source.controller.ts
@@ -0,0 +1,58 @@
+// Uncomment these imports to begin using these cool features!
+
+import {Filter, repository} from '@loopback/repository';
+import {get, getModelSchemaRef, param} from '@loopback/rest';
+import {DietDataSources} from '../../../models';
+import {DietDataSourcesRepository} from '../../../repositories';
+import {StandardJsonResponse} from '../../standardJsonResponse';
+import {StandardOpenApiResponses} from '../../standardOpenApiResponses';
+
+// import {inject} from '@loopback/core';
+
+export class DataSourceController {
+  constructor(
+    @repository(DietDataSourcesRepository)
+    public dietDataSourcesRepository: DietDataSourcesRepository,
+  ) {}
+
+  @get('diet/data-sources', {
+    summary: 'Get diet data sources by country',
+    description:
+      'Get the IDs for the _best_ data sources for use in subsequent requests',
+    tags: ['diet'],
+    responses: new StandardOpenApiResponses(
+      'Array of DietDataSources model instances',
+    )
+      .setDataType('array')
+      .setObjectSchema(getModelSchemaRef(DietDataSources))
+      .toObject(),
+  })
+  async find(
+    @param.query.string('countryId', {
+      description:
+        'ISO 3166-1 alpha-3 code for the country as returned by `/countries`',
+      required: false,
+      example: 'MWI',
+    })
+    countryId: string,
+    @param.query.string('micronutrientId', {
+      description: 'ID for the micronutrient as returned by `/micronutrients`',
+      required: false,
+      example: 'Ca',
+    })
+    micronutrientId: string,
+  ): Promise<object> {
+    const filter: Filter = {
+      where: {
+        countryId: countryId,
+        micronutrientId: micronutrientId,
+      },
+    };
+    const data = await this.dietDataSourcesRepository.find(filter);
+    return new StandardJsonResponse<Array<DietDataSources>>(
+      `${data.length} diet data sources returned.`,
+      data,
+      'DietDataSources',
+    );
+  }
+}
--- src/models/diet-data-sources.model.ts
+++ src/models/diet-data-sources.model.ts
@@ -3,41 +3,101 @@ import {Entity, model, property} from '@loopback/repository';
 @model({
   settings: {
     idInjection: false,
-    postgresql: {schema: process.env.DB_SCHEMA, table: 'diet_data_sources'}
-  }
+    postgresql: {schema: process.env.DB_SCHEMA, table: 'diet_data_sources'},
+  },
 })
 export class DietDataSources extends Entity {
   @property({
     type: 'string',
-    id: true,
-    postgresql: {columnName: 'country_id', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+    required: true,
+    id: 1,
+    description: 'ISO 3166-1 alpha-3 code for the country or territory',
+    postgresql: {
+      columnName: 'country_id',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'NO',
+    },
   })
-  countryId?: string;
+  countryId: string;
 
   @property({
     type: 'string',
+    description: 'ID of the micronutrient',
     id: true,
-    postgresql: {columnName: 'consumption_data_type', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+    postgresql: {
+      columnName: 'micronutrient_id',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'YES',
+    },
+  })
+  micronutrientId?: string;
+
+  @property({
+    type: 'string',
+    description:
+      'The granularity of the consumption data. Either `country` or `household`',
+    id: true,
+    postgresql: {
+      columnName: 'consumption_data_type',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'YES',
+    },
   })
   consumptionDataType?: string;
 
   @property({
     type: 'number',
+    description:
+      'The ID of the consumption data for use in subsequent requests',
     scale: 0,
-    postgresql: {columnName: 'consumption_data_id', dataType: 'integer', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'YES'},
+    postgresql: {
+      columnName: 'consumption_data_id',
+      dataType: 'integer',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: 0,
+      nullable: 'YES',
+    },
   })
   consumptionDataId?: number;
 
   @property({
     type: 'number',
+    description:
+      'The ID of the composition data for use in subsequent requests',
     scale: 0,
-    postgresql: {columnName: 'composition_data_id', dataType: 'integer', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'YES'},
+    postgresql: {
+      columnName: 'composition_data_id',
+      dataType: 'integer',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: 0,
+      nullable: 'YES',
+    },
   })
   compositionDataId?: number;
 
   @property({
     type: 'string',
-    postgresql: {columnName: 'name', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+    description:
+      'Human readable name for the combination of consumption and composition data',
+    postgresql: {
+      columnName: 'name',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'YES',
+    },
   })
   name?: string;
 
@@ -56,4 +116,5 @@ export interface DietDataSourcesRelations {
   // describe navigational properties here
 }
 
-export type DietDataSourcesWithRelations = DietDataSources & DietDataSourcesRelations;
+export type DietDataSourcesWithRelations = DietDataSources &
+  DietDataSourcesRelations;
