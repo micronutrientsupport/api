4771bec43cd94c0c6d204000dce9312dbe91ec09
Bean, Andrew K <andan@bsg.ac.uk>
2022-07-19 19:31:45 +0100

    fix: adjust inputs for household baseline routes

--- src/controllers/diet/availability/dietary-availability.controller.ts
+++ src/controllers/diet/availability/dietary-availability.controller.ts
@@ -193,7 +193,7 @@ export class DietaryAvailabilityController {
     const filter: Filter = {
       where: {
         micronutrientId: micronutrientId,
-        compositionDataId: compositionDataId,
+        // compositionDataId: compositionDataId,
         consumptionDataId: consumptionDataId,
       },
     };
